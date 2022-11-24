6a7f29935c569ca797570b59cf22dc8d4578690c
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-11-24 15:30:40 +0100

    add ingredient controller, view index for list ingredient

---
+++ Controllers/IngredientController.cs
@@ -0,0 +1,22 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class IngredientController : Controller
+    {
+        PizzeriaDbContext db;
+        public IngredientController() : base()
+        {
+            db = new PizzeriaDbContext();
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Ingredient> lista = db.Ingredients.ToList();
+            return View(lista);
+        }
+    }
+}
